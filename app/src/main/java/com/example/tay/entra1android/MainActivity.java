d0a89ed06701045995d7d43801d4929a4c2fdfee
Taysir Al-Shareef Pinero <taysirasp@gmail.com>
2017-11-11 14:51:38 +0100

    Parte 1 Completada: funcionalidad de guardar y mostrar los campos

--- app/src/main/java/com/example/tay/entra1android/MainActivity.java
+++ app/src/main/java/com/example/tay/entra1android/MainActivity.java
@@ -17,6 +17,10 @@ public class MainActivity extends AppCompatActivity {
     //Variable aux para hacer llamadas a la clase MainActivityEvents que contiene el método OnClick
     private MainActivityEvents event;
 
+    //Declaración de variables auxiliares que recogen la información de los camposd del formulario.
+    String name, email, adress;
+    int tlf, day, month, year;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,22 +30,22 @@ public class MainActivity extends AppCompatActivity {
         llamando al constructor de mainActivityEvents
         que recibe por parámetro el MainActivity mediante la sentencia "this".
         */
-        event= new MainActivityEvents(this);
+        event = new MainActivityEvents(this);
         //Fijamos las variables con los elementos visuales (IBOutlet)
 
         //BTN
-        this.btnReturn=(Button) this.findViewById(R.id.btnReturn);
-        this.btnEdit=(Button) this.findViewById(R.id.btnEdit);
-        this.btnSave=(Button) this.findViewById(R.id.btnSave);
+        this.btnReturn = (Button) this.findViewById(R.id.btnReturn);
+        this.btnEdit = (Button) this.findViewById(R.id.btnEdit);
+        this.btnSave = (Button) this.findViewById(R.id.btnSave);
 
         //EDIT TEXT
-        this.txtName=(EditText) this.findViewById(R.id.txtName);
-        this.txtEmail=(EditText) this.findViewById(R.id.txtEmail);
-        this.txtTlf=(EditText) this.findViewById(R.id.txtTlf);
-        this.txtAddress=(EditText) this.findViewById(R.id.txtAddress);
+        this.txtName = (EditText) this.findViewById(R.id.txtName);
+        this.txtEmail = (EditText) this.findViewById(R.id.txtEmail);
+        this.txtTlf = (EditText) this.findViewById(R.id.txtTlf);
+        this.txtAddress = (EditText) this.findViewById(R.id.txtAddress);
 
         //DATEPICKER
-        this.datePicker=(DatePicker) this.findViewById(R.id.datePicker);
+        this.datePicker = (DatePicker) this.findViewById(R.id.datePicker);
 
         /*
         Para fijar el texto de un btn o cualquier View,
@@ -49,9 +53,9 @@ public class MainActivity extends AppCompatActivity {
         escribiendo directamente el string. Pero lo mejor es llamar al xml "strings"
         y ahí especficar el texto del elemento.
         */
-        this.btnReturn.setText(R.string.btnReturn);
-        this.btnReturn.setText(R.string.btnReturn);
-        this.btnSave.setText(R.string.btnSave);
+        this.getBtnReturn().setText(R.string.btnReturn);
+        this.getBtnEdit().setText(R.string.btnEdit);
+        this.getBtnSave().setText(R.string.btnSave);
 
         /*
         Llamámos al método creado por nosotros mismos para establecer que por defecto los componentes
@@ -59,19 +63,50 @@ public class MainActivity extends AppCompatActivity {
         Lo serán cuando hagamos un "onclick" sobre el botón "Editar".
          */
         this.isComponentEditable(false);
-        //Ahora establecemos el listener del "onclick" para el botón "Editar"
-        this.btnEdit.setOnClickListener(this.event);
+        //Ahora establecemos el listener del "onclick" para el botón "Editar", "Guardar" y "Volver"
+        this.getBtnEdit().setOnClickListener(this.getEvent());
+        this.getBtnSave().setOnClickListener(this.getEvent());
+        this.getBtnReturn().setOnClickListener(this.getEvent());
 
     }
 
 
-    public void isComponentEditable(Boolean isTrueOrFalse){
-        this.txtName.setEnabled(isTrueOrFalse);
-        this.txtEmail.setEnabled(isTrueOrFalse);
-        this.txtTlf.setEnabled(isTrueOrFalse);
-        this.txtAddress.setEnabled(isTrueOrFalse);
-        this.datePicker.setEnabled(isTrueOrFalse);
+    public void isComponentEditable(Boolean isTrueOrFalse) {
+        this.getTxtName().setEnabled(isTrueOrFalse);
+        this.getTxtEmail().setEnabled(isTrueOrFalse);
+        this.getTxtTlf().setEnabled(isTrueOrFalse);
+        this.getTxtAddress().setEnabled(isTrueOrFalse);
+        this.getDatePicker().setCalendarViewShown(isTrueOrFalse);
+        this.getDatePicker().setEnabled(isTrueOrFalse);
+        this.getDatePicker().setActivated(isTrueOrFalse);
+        this.getDatePicker().setClickable(isTrueOrFalse);
+        this.getDatePicker().setSelected(isTrueOrFalse);
+
     }
+
+    //Método para recoger la información de los campos del formulario
+
+    public void infoCollect() {
+        //Se hacen uso de los atributos auxiliares creados que recogen la información de los campos
+        this.setName(this.getTxtName().getText().toString());
+        this.setEmail(this.getTxtEmail().getText().toString());
+        this.setTlf(Integer.parseInt(this.getTxtTlf().getText().toString()));
+        this.setAdress(this.getTxtAddress().getText().toString());
+        this.setDay(this.getDatePicker().getDayOfMonth());
+        this.setMonth(this.getDatePicker().getMonth());
+        this.setYear(this.getDatePicker().getYear());
+    }
+
+    public void setInfoCollect() {
+        this.getTxtName().setText(this.getName());
+        this.getTxtEmail().setText(this.getEmail());
+        this.getTxtTlf().setText(String.valueOf(this.getTlf()));
+        this.getTxtAddress().setText(this.getAdress());
+        this.getDatePicker().init(this.getYear(), this.getMonth(), this.getDay(), null);
+    }
+
+
+    //GETTER Y SETTER
 
     public Button getBtnSave() {
         return btnSave;
@@ -137,5 +172,67 @@ public class MainActivity extends AppCompatActivity {
         this.datePicker = datePicker;
     }
 
+    public MainActivityEvents getEvent() {
+        return event;
+    }
 
+    public void setEvent(MainActivityEvents event) {
+        this.event = event;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public int getTlf() {
+        return tlf;
+    }
+
+    public void setTlf(int tlf) {
+        this.tlf = tlf;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
--- app/src/main/java/com/example/tay/entra1android/MainActivityEvents.java
+++ app/src/main/java/com/example/tay/entra1android/MainActivityEvents.java
@@ -6,7 +6,7 @@ import android.view.View;
  * Created by tay on 11/11/17.
  */
 
-public class MainActivityEvents implements View.OnClickListener{
+public class MainActivityEvents implements View.OnClickListener {
     private MainActivity mainActivity; // atributo del MainActivity que usaremos para tratar el onClick
 
     //Constructor para inicializar el MainActivity
@@ -19,11 +19,32 @@ public class MainActivityEvents implements View.OnClickListener{
     @Override
     public void onClick(View view) {
 
-        if(view.getId()==R.id.btnEdit){
+        if (view.getId() == R.id.btnEdit) {
             this.mainActivity.getBtnEdit().setVisibility(View.GONE);// Con Gone a parte de ocultarlo quitamos de la vista para que no ocupe memoria. llamnamos a la clase View para ocultarlo pero en este caso serviría con especificar el parámetro "view"
             this.mainActivity.getBtnSave().setVisibility(View.VISIBLE);
             //Ahora los componentes del View deben de ser editables
             this.mainActivity.isComponentEditable(true);
+
+        } else if (view.getId() == R.id.btnSave) {
+            /*
+            Ahora cuando pinchemos sobre el botón "Guardar" se debe de recoger la información de los campos
+            y guardarse de forma estática en variables auxiliares de tipo atómicas.
+            Para ello llamamos a un método creado en la clase "MainActivity" que se encarga de recoger
+            esta información
+             */
+            this.mainActivity.infoCollect();
+            /*
+            Se realiza el proceso inverso. AL pulsar save se guardan los cambios, se ocutla el botón "Guardar" y se muestra
+            el botón "Editar" de nuevo. Además todos los campos dejan de ser editables.
+             */
+            this.mainActivity.getBtnSave().setVisibility(View.GONE);
+            this.mainActivity.getBtnEdit().setVisibility(View.VISIBLE);
+            this.mainActivity.isComponentEditable(false);
+            /*
+            Por último, se llama a un método creado por nosotros para setear la información que se guardó en las variables locales
+             y de este modo setearselas a los componentes en los campos correspondientes.
+            */
+            this.mainActivity.setInfoCollect();
 
         }
 
